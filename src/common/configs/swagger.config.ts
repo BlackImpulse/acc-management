5a603f6b20330d38449a7442b3e4382c80a0f9ef
BlackImpulse <gmyf32@gmail.com>
2022-07-16 10:39:42 +0300

    add swagger config

---
+++ src/common/configs/swagger.config.ts
@@ -0,0 +1,6 @@
+import { DocumentBuilder } from '@nestjs/swagger';
+
+export const swaggerConfig = new DocumentBuilder()
+  .setTitle('Acc manager')
+  .setVersion('0.1')
+  .build();
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { swaggerConfig } from './common/configs/swagger.config';
+import { SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('swagger', app, document);
 
   const port = configService.get('APP_PORT') || 5000;
   await app.listen(port, () => console.log('Server start on port: ' + port));
